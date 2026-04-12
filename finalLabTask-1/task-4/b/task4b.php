7fa00683458bdaa3ca7fee8dc2a8170ab95ec5fc
Abdullah Al Taieb <abdullah.al.taie.b.02@gmail.com>
2026-04-12 15:06:15 +0600

    Add lab 4 task b

---
+++ finalLabTask-1/task-4/b/task4b.php
@@ -0,0 +1,29 @@
+<?php
+if (isset($_POST['gender'])) {
+    $gender = $_POST['gender'];
+    echo "<p>Your gender is: " . $gender . "</p>";
+} 
+?>
+
+<!doctype html>
+<html>
+  <head>
+    <title>Task 4 - b</title>
+  </head>
+  <body>
+    <form action="" method="post">
+      <fieldset>
+        <legend><b>Gender</b></legend>
+        <input type="radio" name="gender" id="male" value="Male" />
+        <label for="male">Male</label>
+        <input type="radio" name="gender" id="female" value="Female" />
+        <label for="female">Female</label>
+        <input type="radio" name="gender" id="other" value="Other" />
+        <label for="other">Other</label>
+        <br /><br />
+      </fieldset>
+
+      <input type="submit" value="Submit" />
+    </form>
+  </body>
+</html>
