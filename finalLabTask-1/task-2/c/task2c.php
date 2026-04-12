6a5a7adc7cdb55d6205d5a856347e2ebbebe22b0
Abdullah Al Taieb <abdullah.al.taie.b.02@gmail.com>
2026-04-12 14:37:32 +0600

    Add lab 2 task c

---
+++ finalLabTask-1/task-2/c/task2c.php
@@ -0,0 +1,25 @@
+<?php
+if (isset($_POST['email'])) {
+    $email = $_POST['email'];
+    echo "<p>Your email is: " . $email . "</p>";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Task 1 - c</title>
+</head>
+<body>
+
+<form action="" method="post">
+    <fieldset>
+        <legend><b>EMAIL</b></legend>
+        <input type="email" name="email" value="<?php echo $email; ?>">
+        <br><br>
+        <input type="submit" value="Submit">
+    </fieldset>
+</form>
+
+</body>
+</html>
