262f9d376aef3ce2a355310174c62106d7dbe2a4
Abdullah Al Taieb <abdullah.al.taie.b.02@gmail.com>
2026-04-19 20:42:17 +0600

    Add editProfile and name fixes

---
+++ finalLabTask-2/editProfile.php
@@ -0,0 +1,121 @@
+<?php
+include("session.php");
+
+if (!isset($_COOKIE['status'])) {
+    header('location: login.php');
+    exit();
+}
+
+if (!isset($_SESSION['username'])) {
+    header('location: login.php');
+    exit();
+}
+
+$username = $_SESSION['username'];
+$user = $_SESSION['users'][$username];
+$msg = "";
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $gender = isset($_POST['gender']) ? $_POST['gender'] : "";
+    $day = $_POST['day'];
+    $month = $_POST['month'];
+    $year = $_POST['year'];
+
+    if ($name != "" && $email != "" && $gender != "" && $day != "" && $month != "" && $year != "") {
+        $dob = $day . "/" . $month . "/" . $year;
+
+        $_SESSION['users'][$username]['name'] = $name;
+        $_SESSION['users'][$username]['email'] = $email;
+        $_SESSION['users'][$username]['gender'] = $gender;
+        $_SESSION['users'][$username]['dob'] = $dob;
+
+        $user = $_SESSION['users'][$username];
+        $msg = "Profile updated";
+    } else {
+        $msg = "Please fill all fields";
+    }
+}
+
+$dobParts = explode("/", $user['dob']);
+$day = isset($dobParts[0]) ? $dobParts[0] : "";
+$month = isset($dobParts[1]) ? $dobParts[1] : "";
+$year = isset($dobParts[2]) ? $dobParts[2] : "";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Edit Profile</title>
+    <link rel="stylesheet" href="./styles/style.css">
+</head>
+<body>
+
+<div class="main">
+
+    <div class="top">
+        <div class="logo">XCompany</div>
+        <div class="menu">
+            Logged in as <a href="editProfile.php"><?php echo $username; ?></a> |
+            <a href="logout.php">Logout</a>
+        </div>
+    </div>
+
+    <div>
+        <div class="left">
+            <b>Account</b>
+            <hr>
+            <ul>
+                <li><a href="dashboard.php">Dashboard</a></li>
+                <li><a href="viewProfile.php">View Profile</a></li>
+                <li><a href="editProfile.php">Edit Profile</a></li>
+                <li><a href="changeProfilePicture.php">Change Profile Picture</a></li>
+                <li><a href="changePassword.php">Change Password</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+
+        <div class="right">
+            <form method="post">
+                <fieldset>
+                    <legend>EDIT PROFILE</legend>
+
+                    Name<br>
+                    <input type="text" name="name" value="<?php echo $user['name']; ?>">
+                    <hr>
+
+                    Email<br>
+                    <input type="text" name="email" value="<?php echo $user['email']; ?>">
+                    <hr>
+
+                    Gender<br><br>
+                    <input type="radio" name="gender" value="Male" <?php if ($user['gender'] == "Male") echo "checked"; ?>> Male
+                    <input type="radio" name="gender" value="Female" <?php if ($user['gender'] == "Female") echo "checked"; ?>> Female
+                    <input type="radio" name="gender" value="Other" <?php if ($user['gender'] == "Other") echo "checked"; ?>> Other
+                    <hr>
+
+                    Date of Birth<br><br>
+                    <input type="text" name="day" size="2" value="<?php echo $day; ?>"> /
+                    <input type="text" name="month" size="2" value="<?php echo $month; ?>"> /
+                    <input type="text" name="year" size="4" value="<?php echo $year; ?>">
+                    <span>(dd/mm/yyyy)</span>
+                    <hr>
+
+                    <input type="submit" name="submit" value="Submit">
+
+                    <br><br>
+                    <?php echo $msg; ?>
+                </fieldset>
+            </form>
+        </div>
+    </div>
+
+    <div class="footer">
+        Copyright &copy; 2017
+    </div>
+
+</div>
+
+</body>
+</html>
--- finalLabTask-2/viewProfile.php
+++ finalLabTask-2/viewProfile.php
@@ -39,10 +39,10 @@ $user = $_SESSION['users'][$username];
             <hr>
             <ul>
                 <li><a href="dashboard.php">Dashboard</a></li>
-                <li><a href="view_profile.php">View Profile</a></li>
-                <li><a href="edit_profile.php">Edit Profile</a></li>
-                <li><a href="change_picture.php">Change Profile Picture</a></li>
-                <li><a href="change_password.php">Change Password</a></li>
+                <li><a href="viewProfile.php">View Profile</a></li>
+                <li><a href="editProfile.php">Edit Profile</a></li>
+                <li><a href="changeProfilePicture.php">Change Profile Picture</a></li>
+                <li><a href="changePassword.php">Change Password</a></li>
                 <li><a href="logout.php">Logout</a></li>
             </ul>
         </div>
