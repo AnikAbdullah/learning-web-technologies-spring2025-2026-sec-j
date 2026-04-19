1e55d4b4be9c3e5175048f3cf7364cbdfe4230a7
Abdullah Al Taieb <abdullah.al.taie.b.02@gmail.com>
2026-04-19 14:28:08 +0600

    Add homepage with basic css and session

---
+++ finalLabTask-2/index.php
@@ -0,0 +1,26 @@
+<?php include("session.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="./styles/style.css">
+</head>
+<body>
+<div class="main">
+    <div class="top">
+        <div class="logo">XCompany</div>
+        <div class="menu">
+            <a href="index.php">Home</a> |
+            <a href="login.php">Login</a> |
+            <a href="registration.php">Registration</a>
+        </div>
+    </div>
+
+    <div class="body">
+        <h2>Welcome to XCompany</h2>
+    </div>
+
+    <div class="footer">Copyright &copy; 2017</div>
+</div>
+</body>
+</html>
